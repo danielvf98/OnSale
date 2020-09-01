edfd63f758d4c6b1516af5af255961a6aa481bf1
DANIEL ELIECER VASQUEZ FORONDA <danielvasquez230266@correo.itm.edu.co>
2020-08-31 22:16:46 -0500

    Complete categories CRUD

---
+++ OnSale.Common/Entities/Category.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace OnSale.Common.Entities
+{
+    public class Category
+    {
+        public int Id { get; set; }
+
+        [MaxLength(50, ErrorMessage = "The Filed {0} must contain less than {1} characteres")]
+        [Required]
+        public string Name { get; set; }
+
+        [Display(Name = "Image")]
+        public Guid ImageId { get; set; }
+
+        //TODO: Pending to put the correct paths
+        [Display(Name = "Image")]
+        public string ImageFullPath => ImageId == Guid.Empty
+            ? $"https://localhost:44396/images/no-image.png"
+            : $"https://onsale11.blob.core.windows.net/categories/{ImageId}";
+    }
+
+}
